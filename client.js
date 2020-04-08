48a18c53df1b8f39464fcc4b761d3dc5f57d511d
Jeremy Nasato <jeremy.nasato12@gmail.com>
2020-04-08 19:33:00 +0000

    added connection module so function is seperate

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+/**
+ * Establishes connection with the game server
+ */
+const connect = function () {
+  const conn = net.createConnection({
+    host: '10.0.0.209',
+    port: 50541
+  });
+  // interpret incoming data as text
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log("Data: ", data);
+  });
+
+  return conn;
+}
+
+module.exports = { connect };
